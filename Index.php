0f913d6a9002635c21f4b578d6c4cb25c5c0ca7f
clherrington <colin.herrington330@gmail.com>
2021-01-12 07:59:06 -0500

    First steps

---
+++ Index.php
@@ -0,0 +1,21 @@
+<?php
+    include "./View/header.php";
+    include "./View/navbar.php";
+?>
+<div id="buttonGrid" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class='age-grid'>
+        <h2>Age Group</h2>
+        <!-- Button grid form -->            
+            <!-- Button grid -->
+            <div class="age-buttons">
+                <button class="age-button">U8</button>  
+                <button class="age-button">U9</button>
+                <button class="age-button">U10</button>
+                <button class="age-button">U11</button>
+                <button class="age-button">U12</button>
+                <button class="age-button">U13</button>
+                <button class="age-button">U14</button>
+                <button class="age-button">U15</button>
+            </div>
+  </div>
+</div>
---
+++ View/navbar.php
@@ -0,0 +1,4 @@
+
+<div class='navbar-container'>
+    <h2>Navbar</h2>
+</div>
